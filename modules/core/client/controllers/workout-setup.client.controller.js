1209c9689e67f12576553eec4f161c53e9d771ba
jpecoraro342 <jpecoraro342@ufl.edu>
2016-03-05 12:54:41 -0500

    select responds to on change event

--- modules/core/client/controllers/workout-setup.client.controller.js
+++ modules/core/client/controllers/workout-setup.client.controller.js
@@ -9,16 +9,23 @@
 
   function WorkoutSetupController($scope) {
     var vm = this;
+    var numDays = 0;
 
     // Workout setup controller logic
     // ...
     $(function() {
       $('select').material_select();
+      vm.numDays = $('#workout-setup-form select[name=days]').val();
+    });
+
+    $('#workout-setup-form select[name=days]').on('change',function(){
+      console.log('select has changed to:',$(this).val());
+      vm.numDays = $(this).val();
     });
 
     $scope.generateWorkout = function() {
 //      console.log(noDays);
-      console.log($scope.noDays);
-    }
+      console.log(vm.noDays);
+    };
   }
 })();
