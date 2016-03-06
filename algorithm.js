19b66ab408eb703f1f278f6f182acb83926a8d64
Mitchell Irvin <mirvin@ufl.edu>
2016-03-05 21:22:55 -0500

    algorithm added

--- algorithm.js
+++ algorithm.js
@@ -6,71 +6,87 @@ var reps,
 var inputDay = 2;
 var inputExperience = "beginner";
 
-switch(inputDay) {
-	case 1:
-		if(inputExperience == "beginner"){
+var ExperienceLevel = inputExperience;
+var goal = "strength";
+var week = [];
+var numberOfDays = inputDay;
+//days[exercise1,exercise2]
+// var exercise1 = {};
+// exercise1[sets] = 5;
+// exercise1[reps] = 8; 
+// exercise1[id] = exerciseID;
 
-		} else if(inputExperience == "intermediate") {
+//makeWorkout(numberOfDays,week,ExperienceLevel,0);
 
-		} else {
-
+function makeWorkout(numberOfDays, week, ExperienceLevel, goal){		//passed in number of days per week and the week array (holds each day)
+	if(numberOfDays == 1) {
+		switch(goal) {
+			case "strength": 
+				week.push([{"Squats",4,8},{"Bench Press",4,8},{"Deadlift",3,6},{"Rows",4,12}]);
+				break;
+			case "weight loss":
+				week.push([{"Squats",4,8},{"Walking Lunges",4,8},{"Running",30,0}]);
+				break;
+			case "balanced":
+				week.push([{"Squats",4,8},{"Bench Press",4,8},{"Deadlift",3,6},{"Running",15,0}]);
+				break;
+			default:
+				break; 
 		}
-		break;
-	case 2:
-		if(inputExperience == "beginner"){
-			
-		} else if(inputExperience == "intermediate") {
-
-		} else {
-
+	}
+	else {
+		for(int i=0; i < numberOfDays; i++){
+			if(i%2 == 0){
+				makeUpperDay(week, ExperienceLevel, goal);
+			}
+			else if(i%2 == 1) {
+				makeLowerDay(week, ExperienceLevel, goal); 
+			}		
 		}
-		break;
-	case 3:
-		if(inputExperience == "beginner"){
-			
-		} else if(inputExperience == "intermediate") {
-
-		} else {
-
-		}
-		break;
-	case 4:
-		if(inputExperience == "beginner"){
-			
-		} else if(inputExperience == "intermediate") {
-
-		} else {
-
-		}
-		break;
-	case 5:
-		if(inputExperience == "beginner"){
-			
-		} else if(inputExperience == "intermediate") {
-
-		} else {
-
-		}
-		break;
-	default:
+	}
 }
 
-//based off of...
-/*
-var WorkoutSchema = new Schema({
-  workoutProgram: [{
-    weeks: [{
-      days: [{
-        isCompleted: Boolean,
-        ExerciseID: Number,
-        reps: Number,
-        sets: Number
-      }]
-    }],
-    ExperienceLevel: String,
-    type: String,
-    numDays: Number,
-    workoutID: Number
-  }]
-});
-*/
+function makeLowerDay(week, ExperienceLevel, goal){
+	if(goal == "strength"){
+		if(ExperienceLevel == "Beginner"){
+			week.push([{"Walking Lunges",3,10},{"Leg Extensions",3,10},{"Hamstring Curls",3,10}]);
+		} else if(ExperienceLevel == "Intermediate"){
+			week.push([{"Squats",4,8},{"Deadlifts",4,6},{"Hamstring Curls",3,10},{"Calf Extensions",4,12}]);
+		} else {
+			week.push([{"Squats",3,5},{"Deadlifts",3,5},{"Hamstring Curls",4,10},{"Calf Extensions",4,12}]);
+		}
+	} else if(goal == "balanced") {
+		if(ExperienceLevel == "Beginner"){
+			week.push([{"Walking Lunges",3,10},{"Leg Extensions",3,10},{"Elliptical",15,0}]);
+		} else if(ExperienceLevel == "Intermediate"){
+			week.push([{"Squats",4,8},{"Deadlifts",4,6},{"Calf Extensions",4,12},{"Running",15,0}]);
+		} else {
+			week.push([{"Squats",4,8},{"Deadlifts",4,4},{"Calf Extensions",4,12},{"Running",15,0}]);
+		}
+	} else {
+		week.push([{"Squats",4,12},{"Walking Lunges",4,12},{"Running",30,0}]);
+	}
+}
+function makeUpperDay(week, ExperienceLevel, goal){
+	if(goal == "strength"){
+		if(ExperienceLevel == "Beginner"){
+			week.push([{"Bench Press",3,10},{"Lat Pulldowns",3,10},{"Curls",3,10}]);
+		} else if(ExperienceLevel == "Intermediate"){
+			week.push([{"Bench Press",5,5},{"Overhead Press",4,8},{"Pullups",4,6},{"Curls",3,10}]);
+		} else {
+			week.push([{"Bench Press",3,5},{"Overhead Press",4,6},{"Pullups",4,10},{"Curls",3,10}]);
+		}
+	} else if(goal == "balanced"){
+		if(ExperienceLevel == "Beginner"){
+			week.push([{"Bench Press",3,10},{"Lat Pulldowns",3,10},{"Elliptical",10,0}]);
+		} else if(ExperienceLevel == "Intermediate"){
+			week.push([{"Bench Press",5,5},{"Overhead Press",4,8},{"Pullups",4,6},{"Running",15,0}]);
+		} else {
+			week.push([{"Bench Press",3,5},{"Overhead Press",4,6},{"Pullups",4,10},{"Running",15,0}]);
+		}
+	} else {
+		week.push([{"Bench Press",4,12},{"Lat Pulldowns",4,12},{"Running",30,0}]);
+	}
+}
+
+
