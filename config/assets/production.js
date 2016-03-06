9558707c7c545d964dc09696c0d4c1704a511836
jpecoraro342 <jpecoraro342@ufl.edu>
2016-03-06 01:35:37 -0500

    fixing production stuff

--- config/assets/production.js
+++ config/assets/production.js
@@ -7,6 +7,7 @@ module.exports = {
         'public/lib/Materialize/dist/css/materialize.min.css',
       ],
       js: [
+        'https://code.jquery.com/jquery-2.1.1.min.js',
         'public/lib/angular/angular.min.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
