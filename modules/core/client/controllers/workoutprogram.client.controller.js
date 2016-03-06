4c5451201c3419bff17d3be4b6020df4cfaf436d
Drew <dwood12@ufl.edu>
2016-03-06 07:01:48 -0500

    began scrapping data

--- modules/core/client/controllers/workoutprogram.client.controller.js
+++ modules/core/client/controllers/workoutprogram.client.controller.js
@@ -13,7 +13,10 @@
     $http.get('/programs/' + $stateParams.programId).then(function (data, status) {
         console.log(data.data);
         // TODO: Databind the data.data 
-
+        $scope.weeks = data.data.weeks;
+        $scope.numDays = data.data.numDays;
+        $scope.experienceLevel = data.data.experienceLevel;
+        $scope.repeats = new Array($scope.numDays);
       }, function (error) {
           console.log(error);
       });
